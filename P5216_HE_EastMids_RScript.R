7503f3124c51c9968a7e344fe233fea3e664f7c5
Beastwood1 <100147414+Beastwood1@users.noreply.github.com>
2022-06-07 10:57:01 +0100

    latest script

--- P5216_HE_EastMids_RScript.R
+++ P5216_HE_EastMids_RScript.R
@@ -1,17 +1,6 @@
-
+library(devtools)
+devtools::install_github("Azure/Microsoft365R")
+library(Microsoft365R)
+library(AzureGraph)
 
 odb <- get_business_onedrive()
-
-# list files and folders
-od$list_items()
-
-# same as list_items()
-od$list_files()
-od$list_files("Documents")
-
-# upload and download files
-od$download_file("Documents/myfile.docx")
-od$upload_file("somedata.xlsx")
-
-# create a folder
-od$create_folder("Documents/newfolder")
